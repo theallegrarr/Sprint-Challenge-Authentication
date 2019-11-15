e076eb5f5a027fa436088cdce0455beb3ef02121
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-15 09:28:52 +0100

    login route added

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,6 +23,25 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token: token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({error: error.message});
+    });
 });
 
 module.exports = router;
